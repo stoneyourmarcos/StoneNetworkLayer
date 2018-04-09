146cfd5ca9995dd3d366524974f955c67c922890
Marcos Contente <contente.marcos@gmail.com>
2018-04-09 14:39:16 -0300

    Add network encoders

---
+++ StoneNetworkLayer/Encoding/NetworkError.swift
@@ -0,0 +1,5 @@
+public enum NetworkError: String, Error {
+    case parametersNil = "Parameters were nil"
+    case encondingFailed = "Parameter enconding failed"
+    case missingURL = "URL is nil"
+}
