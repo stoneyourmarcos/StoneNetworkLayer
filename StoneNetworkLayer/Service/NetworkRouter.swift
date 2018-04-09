0f6494eaa7d20cd7bd21f71a949670c7c65d4a9e
Marcos Contente <contente.marcos@gmail.com>
2018-04-09 14:37:31 -0300

    Add router services

---
+++ StoneNetworkLayer/Service/NetworkRouter.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public typealias NetworkRouterCompletion = (_ data: Data?,
+    _ response: URLResponse?,
+    _ error: Error?) -> ()
+
+protocol NetworkRouter: class {
+    associatedtype EndPoint: EndPointType
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
+    func cancel()
+}
